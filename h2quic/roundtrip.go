f893b7bcb2e682430b1650c897e39758722c2b3e
Phus Lu <phuslu@hotmail.com>
2017-07-26 19:53:48 +0800

    Add GetClientKey to h2quic.RoundTripper
    Signed-off-by: Phus Lu <phuslu@hotmail.com>

--- h2quic/roundtrip.go
+++ h2quic/roundtrip.go
@@ -38,6 +38,9 @@ type RoundTripper struct {
 	// tls.Client. If nil, the default configuration is used.
 	TLSClientConfig *tls.Config
 
+	// GetClientKey specifies a function to return a clients key string for hostname
+	GetClientKey func(hostname string) string
+
 	// DialAddr specifies an optional function for quic.DailAddr.
 	// If this value is nil, it will default to net.DialAddr for the client.
 	DialAddr func(hostname string, tlsConfig *tls.Config, config *quic.Config) (quic.Session, error)
@@ -120,13 +123,20 @@ func (r *RoundTripper) getClient(hostname string, onlyCached bool) (http.RoundTr
 		r.clients = make(map[string]roundTripCloser)
 	}
 
-	client, ok := r.clients[hostname]
+	var hostnameKey string
+	if r.GetClientKey != nil {
+		hostnameKey = r.GetClientKey(hostname)
+	} else {
+		hostnameKey = hostname
+	}
+
+	client, ok := r.clients[hostnameKey]
 	if !ok {
 		if onlyCached {
 			return nil, ErrNoCachedConn
 		}
 		client = newClient(hostname, r.TLSClientConfig, &roundTripperOpts{DisableCompression: r.DisableCompression, DialAddr: r.DialAddr}, r.QuicConfig)
-		r.clients[hostname] = client
+		r.clients[hostnameKey] = client
 	}
 	return client, nil
 }
