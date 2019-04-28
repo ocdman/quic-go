7fa1d4cad3f19fe6beaca51aa049aa40653e15da
ocdman <ocdman2@gmail.com>
2019-04-28 17:09:43 +0800

    Fix the bug while use acme/autocert: no token cert for ...

--- internal/crypto/cert_chain.go
+++ internal/crypto/cert_chain.go
@@ -109,5 +109,11 @@ func maybeGetConfigForClient(c *tls.Config, sni string) (*tls.Config, error) {
 	}
 	return c.GetConfigForClient(&tls.ClientHelloInfo{
 		ServerName: sni,
+		// TLS 1.3+ cipher suites. Fix the bug while use acme/autocert: no token cert for ...
+		CipherSuites: []uint16{
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+		},
 	})
 }
